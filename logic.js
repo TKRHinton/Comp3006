3ddfa493879d841ef77463b6f6b574a94296b78b
Thomas Hinton <44804463+TKRHinton@users.noreply.github.com>
2020-12-14 20:58:37 +0000

    Added Admin Stats

--- logic.js
+++ logic.js
@@ -17,6 +17,44 @@ function newDate() {
     return date;
 }
 
+function adminStatitics(Users, Games)
+{
+   // let [userNumber, adminNumber, gameNumber] = Array(3).fill(0);
+    let userNumber = 0;
+    let adminNumber = 0;
+    let gameNumber = 0;
+    let lengthNum = 0;
+
+    for (i=0; i<Users.length; i++) {
+        userNumber++;
+
+        if (Users[i].admin == true) {
+            adminNumber++;
+        }
+
+        lengthNum += Users[i].userName.length;
+
+    }
+    lengthNum = lengthNum / userNumber;
+    lengthNum = Math.round(lengthNum * 100) / 100;
+
+    for (i=0; i<Games.length; i++) {
+        gameNumber++;
+    }
+
+    var message = {
+        userNum : userNumber,
+        lenNum : lengthNum,
+        adminNum : adminNumber,
+        gameNum : gameNumber
+    };
+
+    return message;
+}
+
+
+
 module.exports.flipCoin = flipCoin;
 module.exports.addNumbers = addNumbers;
 module.exports.newDate = newDate;
+module.exports.adminStatitics = adminStatitics;
