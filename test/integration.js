2c7312a3743287c5d13a21a9d367a985182db20d
Thomas Hinton <44804463+TKRHinton@users.noreply.github.com>
2020-12-16 22:26:30 +0000

    Intergration Home Test

--- test/integration.js
+++ test/integration.js
@@ -30,3 +30,18 @@ suite("Testing Sum function", function() {
     });
 
 });
+
+suite("Intergration test for date", function() {
+
+    test("Test GET /home", function() {
+        let app = server.app;
+        let date = logic.newDate();
+
+        chai.request(app).get("/home").end(function(error, response) {
+            chai.assert.equal(response.status, 200, "Wrong status code");
+            chai.assert.include(date, response.text, "Wrong response test");
+        });
+
+    });
+
+});
