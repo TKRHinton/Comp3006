fd2c611180dfea7d35d81f8fbe8ea695642d74d3
Thomas Hinton <44804463+TKRHinton@users.noreply.github.com>
2020-12-17 19:12:20 +0000

    Update integration.js

--- test/integration.js
+++ test/integration.js
@@ -19,5 +19,42 @@ suite("Intergration test for date", function() {
 
     });
 
+    test("Test GET /admin", function() {
+        let app = server.app;
+        let date = logic.newDate();
+
+        chai.request(app).get("/admin").end(function(error, response) {
+            chai.assert.equal(response.status, 200, "Wrong status code");
+            chai.assert.include(date, response.text, "Wrong response test");
+        });
+
+    });
+
 });
+
+suite("Intergration test for /admin", async function() {
+
+    test("Testing for games", async function() {
+        let app = server.app;
+        let games = await db.getGames();
+
+        chai.request(app).get("/home").end(function(error, response) {
+            chai.assert.equal(response.status, 200, "Wrong status code");
+            chai.assert.include(games[0].gameName, response.text, "games are not present");
+        });
+
+    });
+
+    test("Testing for users", async function() {
+        let app = server.app;
+        let users = await db.getUsers();
+
+        chai.request(app).get("/home").end(function(error, response) {
+            chai.assert.equal(response.status, 200, "Wrong status code");
+            chai.assert.include(users[0].userName, response.text, "users are not present");
+        });
+
+    });
+});
+
 
