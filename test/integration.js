365d21009675c8c7252b1c8a2f89cc21a137b5b3
Thomas Hinton <44804463+TKRHinton@users.noreply.github.com>
2021-01-07 19:57:58 +0000

    Intergration date testing

--- test/integration.js
+++ test/integration.js
@@ -29,6 +29,28 @@ suite("Intergration test for date", function() {
 
     });
 
+    test("Test GET /games", function() {
+        let app = server.app;
+        let date = logic.newDate();
+
+        chai.request(app).get("/games").end(function(error, response) {
+            chai.assert.equal(response.status, 200, "Wrong status code");
+            chai.assert.include(date, response.text, "Wrong response test");
+        });
+
+    });
+
+    test("Test GET /profile", function() {
+        let app = server.app;
+        let date = logic.newDate();
+
+        chai.request(app).get("/profile").end(function(error, response) {
+            chai.assert.equal(response.status, 200, "Wrong status code");
+            chai.assert.include(date, response.text, "Wrong response test");
+        });
+
+    });
+
 });
 
 
